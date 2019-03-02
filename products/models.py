ac3b05a41db70ea3c320ce996b23e828cbc7d69e
Danel <dtkachenko20@gmail.com>
2019-03-01 23:04:45 -0500

    Product Hunt

--- products/models.py
+++ products/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=250)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    url = models.TextField()
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def summary(self):
+        return self.body[:100]
+
+    def votes_total(self):
+        votes = 0
+        for upvote in Upvote.objects.all():
+            if upvote.product == self:
+                votes += 1
+        return votes
+
+
+class Upvote(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    voter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class ProductCount(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
---
+++ products/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('create', views.create, name="create"),
+    path('<int:product_id>/', views.detail, name="detail"),
+    path('<int:product_id>/upvote', views.upvote, name="upvote"),
+    path('<int:product_id>/upvote1', views.upvote1, name="upvote1"),
+    path('<int:product_id>/delete', views.delete, name="delete"),
+
+]
